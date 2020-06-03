85f7138654e081c99083874e5619139ea089dbf0
Tumen Bairov <bairovt@gmail.com>
2020-06-03 14:51:09 +0900

    fix

---
+++ api/repos.js
@@ -0,0 +1,58 @@
+'use strict';
+const db = require('../lib/arangodb');
+const aql = require('arangojs').aql;
+const Router = require('koa-router');
+
+const router = new Router();
+
+async function getRepos(ctx) {
+  let repos = await db
+    .query(
+      aql`FOR repo IN Repo          
+          SORT repo.name DESC
+          RETURN repo`
+    )
+    .then((cursor) => {
+      return cursor.all();
+    });
+  ctx.body = {
+    repos,
+  };
+}
+
+async function getRepo(ctx) {
+  const { _key } = ctx.params;
+  const repo = await db.collection('Repo').document(_key);
+  ctx.body = {
+    repo,
+  };
+}
+
+async function getRepoProducts(ctx) {
+  const { _key } = ctx.params;
+
+  const repo = await db.collection('Repo').document(_key);
+
+  const products = await db
+    .query(
+      aql`FOR shift IN shifts
+          LET product = DOCUMENT(shift.product_id)                    
+          RETURN {
+            name: DOCUMENT(product.nomen_id), 
+            qty: shift.qty,
+            seats: shift.seats 
+          }`
+    )
+    .then((cursor) => {
+      return cursor.all();
+    });
+
+  ctx.body = {
+    repo,
+    products,
+  };
+}
+
+router.get('/', getRepos).get('/:_key', getRepo).get('/:_key/products', getRepoProducts);
+
+module.exports = router.routes();
